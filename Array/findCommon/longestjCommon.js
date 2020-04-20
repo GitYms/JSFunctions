0c2e51342adac908e63e38eb8d22f80c679fe0e7
yms <mi_msyuan@outlook.com>
2020-04-20 17:31:47 +0800

    temp: find common prefix from array

---
+++ Array/findCommon/longestjCommon.js
@@ -0,0 +1,11 @@
+/*
+  寻找数组中的最长前缀并输入，没有则输入‘’；
+    ['flower','flow','flight'] ==> 'fl'
+    ['dog','racecar','car'] ==> ''
+*/
+
+function find_longest_prefix(arr) {
+  let result = '';
+  
+  return result;
+}
